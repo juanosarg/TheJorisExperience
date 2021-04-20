d81987f9be7bf4f0011fa5224b9894f8bebdb796
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-04-20 14:51:01 +0200

    v0.1

---
+++ 1.2/Source/TheJorisExperience/TheJorisExperience/Harmony/TrainableUtility_TamenessCanDecay.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Verse.AI;
+using RimWorld.Planet;
+
+
+
+namespace TheJorisExperience
+{
+
+
+    [HarmonyPatch(typeof(TrainableUtility))]
+    [HarmonyPatch("TamenessCanDecay")]
+
+    public static class TheJorisExperience_TrainableUtility_TamenessCanDecay_Patch
+    {
+        [HarmonyPrefix]
+        public static bool RemoveTamenessDecayCheck(ThingDef def)
+
+        {
+            if ((def.defName.Contains("JECom_JorisAstley")))
+            {
+                return false;
+
+            }
+            else return true;
+        }
+    }
+
+
+}
